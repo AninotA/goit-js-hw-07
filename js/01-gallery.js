eb2fcc31c86221b24184e343e2b4ed38f2507c24
Antonina Buchak <antoninabucak@gmail.com>
2022-11-04 14:52:37 +0200

    Update task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,44 +2,51 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-console.log(basicLightbox);
+// console.log(basicLightbox);
 
 const imgList = document.querySelector(".gallery");
-// const newGallery = createElements(galleryItems);
-// imgList.innerHTML = newGallery;
 
-// imgList.innerHTML = markup;
 
-imgList.addEventListener("click", onClick);
 
-const createMarkup = galleryItems.map(({ preview, original, description }) => 
-`<div class ="gallery__item">
+const createMarkup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<div class ="gallery__item">
 <a class="gallery__link" href="${original}">
-<img class="gallery__image" 
+<img class="gallery__image"
 src="${preview}"
 data-source="${original}"
 alt="${description}"/>
 </a>
-</div>`) 
- 
-imgList.insertAdjacentHTML('beforeend', createMarkup.join('')) 
+</div>`
+);
 
-  
+imgList.insertAdjacentHTML("beforeend", createMarkup.join(""));
 
-
-
-let instance = "";
-function onClick(event) {
+const clickImg = (document.querySelectorAll("gallery__image").onClick = (
+  event
+) => {
   event.preventDefault();
-   
-  instance = basicLightbox.create(`<img src=${event.target.dataset.source}>`);
-  instance.show();
-  document.addEventListener("keydown", onEscapeCloseModal);
-}
-function onEscapeCloseModal(evt) {
-  if (evt.code === "Escape") {
-    instance.close();
-    document.removeEventListener("keydown", onEscapeCloseModal);
-  }
-}
 
+  const html = `<img src="${event.target.dataset.source}" alt="${event.target.dataset.description}">`;
+  const instance = basicLightbox.create(html, {
+    onShow: () => {
+      document.addEventListener("keydown", onEscapeCloseModal);
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", onEscapeCloseModal);
+    },
+  });
+
+  if (event.target.nodeName === "IMG") {
+    instance.show();
+  }
+
+  function onEscapeCloseModal(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+});
+imgList.addEventListener("click", clickImg);
+
+console.log(galleryItems);
